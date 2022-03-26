b2430518225bcf150a0955e2301ae0157d40c0b0
Hsin-Hao Yu <hsinhaohh.yu@gmail.com>
2022-03-26 22:32:17 +1100

    experiment with saving codeblock results to HTML

--- src/test_tic_tac_toe.py
+++ src/test_tic_tac_toe.py
@@ -1,23 +1,23 @@
-from tic_tac_toe import init_board, moves, static_eval, display_board
-from tic_tac_toe import who_plays, posinf, neginf, gametree, prune, evaluate1, won
-from tic_tac_toe import static_eval_state
-from lazy_utils import tree_size, tree_depth, maptree, tree_labels
-import pytest
+def test_who_plays():
+    b = init_board()
+    assert who_plays(b) == 0
+
+    b[1] = 0
+    assert who_plays(b) == 1
 
 
-def test_gametree():
-    print("\n\n## test_gametree: no pruning")
-    b0 = init_board()
-    t = gametree(b0)
-    d = tree_depth(t)
-    print("depth=", d)
+def test_moves():
+    # there should be no legal move for 0 because 1 has already won
+    b = [1, 0, 0, 1, 0, None, 1, None, None]
+    assert moves(b) is None
 
+    # there should be no legal move for 0 because 0 has already won
+    b = [1, 0, 0, 1, 0, None, None, 0, 1]
+    assert moves(b) is None
 
-def test_gametree2():
-    b0 = init_board()
-    t = gametree(b0)
-    s = tree_size(t)
-    print("size=", s)
+    # A draw. the board is full
+    b = [1, 0, 1, 0, 0, 1, 0, 1, 0]
+    assert moves(b) is None
 
 
 def test_prune():
@@ -33,6 +33,44 @@ def test_prune2():
     t = prune(gametree(b0))
     s = tree_size(t)
     print("size=", s)
+
+
+def test_game_tree_structure():
+    # Since player 0 has won, there should be no subtrees
+    b = [1, 0, 0, 1, 0, None, None, 0, 1]
+    assert won(b, 0) == True
+    t = gametree(b)
+    assert t[1] is None
+
+    # Since player 1 has won, there should be no subtrees
+    b = [1, 0, 0, 1, 0, None, 1, None, None]
+    assert won(b, 1) == True
+    t = gametree(b)
+    assert t[1] is None
+
+    # This is a draw. There should be no subtrees
+    b = [1, 0, 1, 0, 0, 1, 0, 1, 0]
+    assert won(b, 0) == False
+    assert won(b, 1) == False
+    t = gametree(b)
+    assert t[1] is None
+
+
+def test_static_eval_winning_condition():
+    # evaluate for player 0
+    eval_0 = static_eval(0)
+    # evaluate for player 1
+    eval_1 = static_eval(1)
+
+    # player 0 won
+    b = [1, 0, 0, 1, 0, None, None, 0, 1]
+    assert eval_0(b) == posinf
+    assert eval_1(b) == neginf
+
+    # player 1 won
+    b = [1, 0, 0, 1, 0, None, 1, None, None]
+    assert eval_0(b) == neginf
+    assert eval_1(b) == posinf
 
 
 def test_static_eval():
@@ -62,6 +100,23 @@ def test_static_eval():
     show_freq(freq(t))
 
 
+def test_gametree_evaluation():
+    # player 0 has won
+    b = [1, 0, 0, 1, 0, None, None, 0, 1]
+    (score, subtrees) = maptree(static_eval(0), prune(gametree(b)))
+    assert subtrees is None and score == posinf
+
+    # player 1 has won
+    b = [1, 0, 0, 1, 0, None, 1, None, None]
+    (score, subtrees) = maptree(static_eval(0), prune(gametree(b)))
+    assert subtrees is None and score == neginf
+
+    # This is a draw
+    b = [1, 0, 1, 0, 0, 1, 0, 1, 0]
+    (score, subtrees) = maptree(static_eval(0), prune(gametree(b)))
+    assert subtrees is None and score == 0
+
+
 def test_tree_eval():
     print("## test_tree_eval()")
     b = [1, 0, 0, None, 0, None, 1, None, None]
@@ -80,80 +135,3 @@ def test_tree_eval():
 
 
 #@pytest.mark.skip(reason="skip skip skip")
-
-
-def test_who_plays():
-    b = init_board()
-    assert who_plays(b) == 0
-
-    b[1] = 0
-    assert who_plays(b) == 1
-
-
-def test_moves():
-    # there should be no legal move for 0 because 1 has already won
-    b = [1, 0, 0, 1, 0, None, 1, None, None]
-    assert moves(b) is None
-
-    # there should be no legal move for 0 because 0 has already won
-    b = [1, 0, 0, 1, 0, None, None, 0, 1]
-    assert moves(b) is None
-
-    # the board is full
-    b = [1, 0, 1, 0, 0, 1, 0, 1, 0]
-    assert moves(b) is None
-
-
-def test_static_eval_winning_condition():
-    # evaluate for player 0
-    eval_0 = static_eval(0)
-    # evaluate for player 1
-    eval_1 = static_eval(1)
-
-    # player 0 won
-    b = [1, 0, 0, 1, 0, None, None, 0, 1]
-    assert eval_0(b) == posinf
-    assert eval_1(b) == neginf
-
-    # player 1 won
-    b = [1, 0, 0, 1, 0, None, 1, None, None]
-    assert eval_0(b) == neginf
-    assert eval_1(b) == posinf
-
-
-def test_game_tree_structure():
-    # Since player 0 has won, there should be no subtrees
-    b = [1, 0, 0, 1, 0, None, None, 0, 1]
-    assert won(b, 0) == True
-    t = gametree(b)
-    assert t[1] is None
-
-    # Since player 1 has won, there should be no subtrees
-    b = [1, 0, 0, 1, 0, None, 1, None, None]
-    assert won(b, 1) == True
-    t = gametree(b)
-    assert t[1] is None
-
-    # This is a draw. There should be no subtrees
-    b = [1, 0, 1, 0, 0, 1, 0, 1, 0]
-    assert won(b, 0) == False
-    assert won(b, 1) == False
-    t = gametree(b)
-    assert t[1] is None
-
-
-def test_gametree_evaluation():
-    # player 0 has won
-    b = [1, 0, 0, 1, 0, None, None, 0, 1]
-    (score, subtrees) = maptree(static_eval(0), prune(gametree(b)))
-    assert subtrees is None and score == posinf
-
-    # player 1 has won
-    b = [1, 0, 0, 1, 0, None, 1, None, None]
-    (score, subtrees) = maptree(static_eval(0), prune(gametree(b)))
-    assert subtrees is None and score == neginf
-
-    # This is a draw
-    b = [1, 0, 1, 0, 0, 1, 0, 1, 0]
-    (score, subtrees) = maptree(static_eval(0), prune(gametree(b)))
-    assert subtrees is None and score == 0
