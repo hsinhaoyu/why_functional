a03341ec2706855d42d659a6f079ebc8b08b8fe7
Hsin-Hao Yu <hsinhaohh.yu@gmail.com>
2022-03-29 17:38:02 +1100

    foldtree updates: types, more tests

--- src/foldtree.py
+++ src/foldtree.py
@@ -1,17 +1,19 @@
-from typing import Tuple, Callable, Any, List, Union
+from typing import Tuple, Callable, Any, List, Union, NamedTuple
 import operator
 
+Node = NamedTuple('Node', [('label', Any), ('subtrees', List)])
 
-def foldtree(f: Callable, g: Callable, a: Any, t: Union[Tuple, List]):
+
+def foldtree(f: Callable, g: Callable, a: Any, t: Union[Node, List]) -> Any:
     """Apply two functions (f and g) of two arguments to transform a tree.
-    f: combine the label of a node to its subtrees
-    g: combine the subtrees of a node
+    f: fold a node's label to the folded subtrees
+    g: fold a list of subtrees
     a: an initial constant
     t: a tree, a list of subtrees, or []
     """
     if t == []:
         return a
-    elif isinstance(t, tuple):
+    elif isinstance(t, Node):
         (label, subtrees) = t
         return f(label, foldtree(f, g, a, subtrees))
     else:
@@ -21,14 +23,14 @@ def foldtree(f: Callable, g: Callable, a: Any, t: Union[Tuple, List]):
         return g(foldtree(f, g, a, subtree), foldtree(f, g, a, rest))
 
 
-def sumtree(t: Tuple) -> int:
+def sumtree(t: Node) -> int:
     """Sum all labels in a tree."""
     f = operator.add
     g = operator.add
     return foldtree(f, g, 0, t)
 
 
-def tree_labels(t):
+def tree_labels(t: Node):
     """Collect all labels of a tree into a list."""
 
     def f(label: Any, folded_subtrees: List) -> List:
@@ -40,24 +42,24 @@ def tree_labels(t):
     return foldtree(f, g, [], t)
 
 
-def maptree(func: Callable, t: Tuple) -> Tuple:
+def maptree(func: Callable, t: Node) -> Node:
     """Map a function to all labels in a tree.
     Return a new tree.
     """
 
-    def f(label: Any, folded_subtrees: List) -> Tuple:
-        return (func(label), folded_subtrees)
+    def f(label: Any, folded_subtrees: List) -> Node:
+        return Node(func(label), folded_subtrees)
 
-    def g(folded_first: Any, folded_rest: List) -> List:
+    def g(folded_first: Node, folded_rest: List) -> List:
         return [folded_first] + folded_rest
 
     return foldtree(f, g, [], t)
 
 
-def tree_size(t: Tuple) -> int:
+def tree_size(t: Node) -> int:
     """Return the number of nodes in a tree"""
 
-    def f(label, folded_subtrees: int):
+    def f(label: int, folded_subtrees: int) -> int:
         return 1 + folded_subtrees
 
     def g(folded_first: int, folded_rest: int) -> int:
@@ -66,7 +68,7 @@ def tree_size(t: Tuple) -> int:
     return foldtree(f, g, 0, t)
 
 
-def tree_depth(t: Tuple) -> int:
+def tree_depth(t: Node) -> int:
     """Returns the maximal depth of nodes in the tree"""
 
     def f(label: Any, folded_subtrees: int):
--- src/test_foldtree.py
+++ src/test_foldtree.py
@@ -1,22 +1,33 @@
 from foldtree import *
 
-my_tree2 = (1, [(2, [(3, []), (4, [(5, []), (6, [(7, [])])]), (8, [(9, [])])]),
-                (10, [(11, [])])])
+my_tree2 = Node(1, [
+    Node(2, [
+        Node(3, []),
+        Node(4, [Node(5, []), Node(6, [Node(7, [])])]),
+        Node(8, [Node(9, [])])
+    ]),
+    Node(10, [Node(11, [])])
+])
 
-my_tree = (1, [(2, []), (3, [(4, [])])])
+my_tree = Node(1, [Node(2, []), Node(3, [Node(4, [])])])
 
 
 def test_sumtree():
+    assert sumtree(Node(1, [])) == 1
     assert sumtree(my_tree) == 10
     assert sumtree(my_tree2) == sum(range(1, 12))
 
 
 def test_tree_labels():
+    assert tree_labels(Node(1, [])) == [1]
     assert tree_labels(my_tree) == [1, 2, 3, 4]
     assert tree_labels(my_tree2) == list(range(1, 12))
 
 
 def test_maptree():
+    res = maptree(lambda x: -1 * x, Node(1, []))
+    assert res == Node(-1, [])
+
     res = maptree(lambda x: -1 * x, my_tree)
     res = tree_labels(res)
     assert res == [-1, -2, -3, -4]
@@ -28,15 +39,15 @@ def test_maptree():
 
 def test_tree_size():
     assert tree_size(my_tree) == 4
-    assert tree_size((1, [])) == 1
+    assert tree_size(Node(1, [])) == 1
     assert tree_size(my_tree2) == 11
 
 
 def test_tree_depth():
-    t = (1, [])
+    t = Node(1, [])
     assert tree_depth(t) == 1
 
-    t = (1, [(2, [])])
+    t = Node(1, [Node(2, [])])
     assert tree_depth(t) == 2
 
     assert tree_depth(my_tree) == 3
